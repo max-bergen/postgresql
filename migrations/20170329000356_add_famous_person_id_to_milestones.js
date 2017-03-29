8d6c0ba94c9637572eb0f24e8b624ee58b5de75f
max-bergen <maxbergen1234@gmail.com>
2017-03-29 00:28:24 +0000

    add famous person to milestones

---
+++ migrations/20170329000356_add_famous_person_id_to_milestones.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  knex.schema.table('milestones', function (table) {
+    table.integer('famous_people_id').unsigned()
+    table.foreign('famous_people_id').references('famous_people.id')
+})
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function (table) {
+      table.dropForeign('famous_people_id')
+      table.dropColumn('famous_people_id')
+    })
+  ])
+};
